4715851d9c756574073b59007ecb754d602a7619
LILYREYES <lilia.lnci@hotmail.com>
2025-09-19 02:11:12 -0600

    Py, Html, Css Sustentable

---
+++ Arreglo.js
@@ -0,0 +1,25 @@
+En la carrera se muestran los participantes en las siguientes posiciones 
+
+let participantes = ["Elena", "Carlos", "Javier", "Laura", "Miguel", "Patricia"];
+
+
+console.log("Clasificación actual:");
+console.log(participantes);
+
+let indexLaura = participantes.indexOf("Laura");
+let indexJavier = participantes.indexOf("Javier");
+
+if (indexLaura > indexJavier) {
+    [participantes[indexLaura], participantes[indexJavier]] = [participantes[indexJavier], participantes[indexLaura]];
+}
+let indexPatricia = participantes.indexOf("Patricia");
+if (indexPatricia !== -1) {
+    participantes.splice(indexPatricia, 1); 
+}
+let indexCarlos = participantes.indexOf("Carlos");
+participantes.splice(indexCarlos, 0, "Raúl", "Sofía");
+
+participantes.unshift("Carmen");
+
+console.log("Clasificación actualizada:");
+console.log(participantes);
